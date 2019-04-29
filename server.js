5ae0d1689c3d0e100610c02f97ab6d63afcee23e
kaphasouvor <khambayphasouvor.student@careerdevs.com>
2019-04-29 17:53:39 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('./data.js');
 
 // function call of express..
 const app = express();
-const port = 8080; 
+const port = process.env.PORT || 8000; 
 // process.env.PORT || 5000; // if running file locally it will not have process.evn.PORT but runs 5000 instead. Testing.....
 
 console.log(fs);
@@ -12,8 +12,8 @@ console.log(fs);
 // .get api call. Anonymous function: request.
 // and respose object and using send method(hello world)
 app.get('/', function (req, res){
-    const userName = req.query.user;
-    res.send('Hello ' + userName + '!')
+    const s_word = req.query.word;
+    res.send('Word you entered' + s_word + '!')
 });
 
 // When port is use. It needs to know which door to go into. ex:3000
